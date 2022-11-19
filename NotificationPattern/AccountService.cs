2d6f66bb2e12c77b1f82e0ff3de488fa27fb6d64
Lucas Fogliarini <lucasfogliarini@gmail.com>
2022-11-19 01:42:53 -0300

    ajuste no transfer

--- NotificationPattern/AccountService.cs
+++ NotificationPattern/AccountService.cs
@@ -14,8 +14,13 @@ namespace NotificationPattern
         public void Transfer(TransferInput transfer)
         {
             transfer.FromAccount = MockDb.Accounts.FirstOrDefault(e => e.Cpf == transfer.FromAccountCpf);
-            transfer.ToAccount = MockDb.Accounts.FirstOrDefault(e => e.Cpf == transfer.FromAccountCpf);
+            transfer.ToAccount = MockDb.Accounts.FirstOrDefault(e => e.Cpf == transfer.ToAccountCpf);
             _validator.Validate<TransferContract>(transfer);
+            if(_validator.Valid)
+            {
+                transfer.FromAccount.Balance -= transfer.Value;
+                transfer.ToAccount.Balance += transfer.Value;
+            }
         }
     }
 }
